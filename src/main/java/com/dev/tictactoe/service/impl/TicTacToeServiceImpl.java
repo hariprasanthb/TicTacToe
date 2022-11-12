66180e138c2f446baaa7ea2902ba783fc9c57056
hariprasanthb <hariprasanthb@lucid-green.com>
2022-11-12 22:37:42 +0530

    TestCase 2: Players cannot play on a played position coverage commit

--- src/main/java/com/dev/tictactoe/service/impl/TicTacToeServiceImpl.java
+++ src/main/java/com/dev/tictactoe/service/impl/TicTacToeServiceImpl.java
@@ -2,6 +2,8 @@ package com.dev.tictactoe.service.impl;
 
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.dev.tictactoe.constants.TicTacToeAppRunningStatus;
 import com.dev.tictactoe.constants.TicTacToePlayer;
+import com.dev.tictactoe.constants.TicTacToePosition;
 import com.dev.tictactoe.model.TicTacToeInput;
 import com.dev.tictactoe.model.TicTacToeResponse;
 import com.dev.tictactoe.service.TicTacToeService;
@@ -27,6 +30,15 @@ public class TicTacToeServiceImpl implements TicTacToeService
 		if (ticTacToeUserInput.get(0).getPlayer().equals(TicTacToePlayer.O)) {
 			logger.error("X always goes first");
 			message = "X always goes first";
+			status = TicTacToeAppRunningStatus.FAIL;
+		}
+		
+		Map<TicTacToePosition, List<TicTacToeInput>> ticTacToeInputByPosition = ticTacToeUserInput.stream()
+				.collect(Collectors.groupingBy(TicTacToeInput::getPosition));
+		boolean isSamePositionFilled = ticTacToeInputByPosition.entrySet().stream().anyMatch(input -> input.getValue().size() > 1);
+		if(isSamePositionFilled) {
+			logger.error("Players cannot play on a played position.");
+			message = "Players cannot play on a played position.";
 			status = TicTacToeAppRunningStatus.FAIL;
 		}
 		
--- src/test/java/com/dev/tictactoe/TicTacToeApplicationTests.java
+++ src/test/java/com/dev/tictactoe/TicTacToeApplicationTests.java
@@ -2,10 +2,16 @@ package com.dev.tictactoe;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.Alphanumeric;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -19,6 +25,7 @@ import com.dev.tictactoe.service.TicTacToeService;
 import com.dev.tictactoe.service.impl.TicTacToeServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TicTacToeApplicationTests {
 	
 	@Mock
@@ -31,9 +38,20 @@ class TicTacToeApplicationTests {
     }
  
     @Test
-    void isFirstInputValid() {
+    @Order(1)
+    void caseA_isFirstInputValid() {
     	TicTacToeInput ticTacToeInput = new TicTacToeInput(TicTacToePosition.A1, TicTacToePlayer.O);
     	TicTacToeResponse response = ticTacToeService.validateUserInput(Arrays.asList(ticTacToeInput));
+    	assertEquals(response.getStatus(), TicTacToeAppRunningStatus.FAIL);
+    }
+    
+    @Test
+    @Order(2)
+    void caseB_isFiledPositionGiven() {
+    	List<TicTacToeInput> ticTacToeInputs = new ArrayList<>();
+    	ticTacToeInputs.add(new TicTacToeInput(TicTacToePosition.A1, TicTacToePlayer.X));
+    	ticTacToeInputs.add(new TicTacToeInput(TicTacToePosition.A1, TicTacToePlayer.O));
+    	TicTacToeResponse response = ticTacToeService.validateUserInput(ticTacToeInputs);
     	assertEquals(response.getStatus(), TicTacToeAppRunningStatus.FAIL);
     }
 }
